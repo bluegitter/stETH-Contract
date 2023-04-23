538d9d6f22dcd0c76197d197b2184ba19249fd6a
yanfei <2082307@qq.com>
2023-04-23 16:15:43 +0800

    first commit

--- contracts/UpgradeableToken.sol
+++ contracts/UpgradeableToken.sol
@@ -3,9 +3,12 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/token/ERC20/utils/SafeERC20Upgradeable.sol";
 import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
 
 contract UpgradeableToken is Initializable, ERC20Upgradeable {
+    using SafeERC20Upgradeable for IERC20Upgradeable;
+
     function initialize(
         string memory name,
         string memory symbol,
